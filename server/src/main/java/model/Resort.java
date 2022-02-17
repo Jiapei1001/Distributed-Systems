5efb6d3bfd8178765f29bcef89023d7320935bf7
Jiapei1001 <jiapeili@live.com>
2022-02-16 21:25:30 -0800

    include resort servlet

--- server/src/main/java/model/Resort.java
+++ server/src/main/java/model/Resort.java
@@ -1,12 +1,21 @@
 package model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Resort {
     private Integer resortID;
     private String resortName;
+    private Set<Season> seasons;
 
     public Resort(Integer resortID, String resortName) {
         this.resortID = resortID;
         this.resortName = resortName;
+        this.seasons = new HashSet<>();
+    }
+
+    public void addSeason(Season s) {
+        this.seasons.add(s);
     }
 
     public Integer getResortID() {
--- server/src/main/java/servlet/ResortServlet.java
+++ server/src/main/java/servlet/ResortServlet.java
@@ -4,20 +4,21 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import javax.servlet.annotation.*;
 import java.io.IOException;
 import model.Message;
-import model.LiftRide;
-import model.SkierVertical;
+import model.Resort;
+import model.ResortReport;
+import model.Season;
 
-
-@WebServlet(name = "SkierServlet", value = "/SkierServlet")
-public class SkierServlet extends HttpServlet {
+@WebServlet(name = "ResortServlet", value = "/ResortServlet")
+public class ResortServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -27,7 +28,50 @@ public class SkierServlet extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
 
         String url = request.getRequestURI();
-        if (url == null || url.length() == 0) {
+        // url = /resorts
+        if (url == null || url.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            List<Resort> resorts = new ArrayList<>();
+            resorts.add(new Resort(1, "Seattle Resort"));
+            resorts.add(new Resort(2, "Olympic Resort"));
+            response.getWriter().write(gson.toJson(resorts));
+            return;
+        }
+
+        String[] urlPath = url.split("/");
+        if (!isUrlValid(urlPath)) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write(gson.toJson(new Message("Invalid inputs")));
+            return;
+        }
+
+        // valid
+        // urlParts = [, 1, seasons]
+        if (urlPath.length == 3) {
+            // TODO: process to get the seasons by the input resortID
+            Set<Season> seasons = new HashSet<>();
+            seasons.add(new Season("2020"));
+            seasons.add(new Season("2021"));
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().write(gson.toJson(seasons));
+        }
+        // urlParts = [, 1, seasons, 2019, day, 1, skiers]
+        else if (urlPath.length == 7) {
+            // TODO: process to get the number of skiers at resort/season/day
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().write(gson.toJson(new ResortReport("Mission Ridge", 78999)));
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Gson gson = new Gson();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String url = request.getRequestURI();
+        if (url == null || url.isEmpty()) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             response.getWriter().write(gson.toJson(new Message("Invalid inputs")));
             return;
@@ -41,74 +85,29 @@ public class SkierServlet extends HttpServlet {
         }
 
         // valid
-        if (urlPath[2].equals("vertical")) {
-            // TODO: business logic to calculate total vertical for the skier for specified seasons at the specified resort
-            SkierVertical skier1 = new SkierVertical(1, "2022-Winter", "23", 102, 202);
-            SkierVertical skier2 = new SkierVertical(1, "2021-Winter", "86", 102, 306);
-
-            Map<String, List<SkierVertical>> verticals = new HashMap<>();
-            verticals.computeIfAbsent("resorts", a -> new ArrayList<SkierVertical>()).add(skier1);
-            verticals.computeIfAbsent("resorts", a -> new ArrayList<SkierVertical>()).add(skier2);
-
-            response.getWriter().write(gson.toJson(verticals));
-            response.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            // urlParts = [, 1, seasons, 2019, day, 1, skier, 123]
-            // TODO: get the total vertical for the skier for the specified ski day
-            response.getWriter().write("34507");
-            response.setStatus(HttpServletResponse.SC_OK);
-        }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        Gson gson = new Gson();
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-
-        String url = request.getRequestURI();
-        if (url == null || url.length() == 0) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            response.getWriter().write(gson.toJson(new Message("Invalid inputs")));
-            return;
-        }
-
-        String[] urlPath = url.split("/");
-        if (!isUrlValid(urlPath) || !urlPath[2].equals("seasons")) {
-            response.getWriter().write(gson.toJson(new Message("Invalid inputs")));
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
-        // valid ride
-        // urlParts = [, 1, seasons, 2019, day, 1, skier, 123]
-        // convert request body to a map
+        // urlParts = [, 1, seasons]
+        // get request body
         Type type = new TypeToken<Map<String, String>>(){}.getType();
         Map<String, String> reqBody = gson.fromJson(request.getReader(), type);
 
-        LiftRide ride = new LiftRide(
-                Integer.parseInt(urlPath[1]),
-                urlPath[3],
-                urlPath[5],
-                Integer.parseInt(urlPath[7]),
-                Long.parseLong(reqBody.get("time")),
-                Integer.parseInt(reqBody.get("liftID")),
-                Long.parseLong(reqBody.get("waitTime")));
-        // TODO: process lift ride
+        Season season = new Season(reqBody.get("year"));
+        // TODO: get the specific resort by resortID, if not found, return 404
+        Resort resort = new Resort(1, "Seattle resort");
+        resort.addSeason(season);
 
         response.setStatus(HttpServletResponse.SC_CREATED);
+        response.getWriter().write(gson.toJson(resort));
     }
 
     // validate the request url path according to the API spec
     private boolean isUrlValid(String[] urlPath) {
-        // urlPath  = "/1/vertical"
+        // urlPath  = "/1/seasons"
         if (urlPath.length == 3) {
-            return ServerUtil.isNumeric(urlPath[1]) && urlPath[2].equals("vertical");
+            return ServerUtil.isNumeric(urlPath[1]) && urlPath[2].equals("seasons");
         }
-        // urlPath  = "/1/seasons/2019/day/1/skier/123"
-        // urlParts = [, 1, seasons, 2019, day, 1, skier, 123]
-        if (urlPath.length == 8) {
+        // urlPath  = "/1/seasons/2019/day/1/skiers"
+        // urlParts = [, 1, seasons, 2019, day, 1, skiers]
+        if (urlPath.length == 7) {
             return ServerUtil.isNumeric(urlPath[1])
                     && (urlPath[2].equals("seasons"))
                     && ServerUtil.isNumeric(urlPath[3])
@@ -116,8 +115,7 @@ public class SkierServlet extends HttpServlet {
                     && ServerUtil.isNumeric((urlPath[5]))
                     && Integer.parseInt(urlPath[5]) >= 1
                     && Integer.parseInt(urlPath[5]) <= 365
-                    && (urlPath[6].equals("skier"))
-                    && ServerUtil.isNumeric(urlPath[7]);
+                    && (urlPath[6].equals("skiers"));
         }
 
         return false;
