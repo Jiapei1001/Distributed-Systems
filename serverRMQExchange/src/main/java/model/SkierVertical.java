c2dee3fa3b9429bed094bf756e07478502297552
Jiapei1001 <jiapeili@live.com>
2022-04-09 15:06:17 -0700

    server exchange

---
+++ serverRMQExchange/src/main/java/model/SkierVertical.java
@@ -0,0 +1,19 @@
+package model;
+
+public class SkierVertical {
+
+    Integer resortID;
+    String seasonID;
+    String dayID;
+    Integer skierID;
+    Integer vertical;
+
+    public SkierVertical(Integer resortID, String seasonID, String dayID, Integer skierID,
+            Integer vertical) {
+        this.resortID = resortID;
+        this.seasonID = seasonID;
+        this.dayID = dayID;
+        this.skierID = skierID;
+        this.vertical = vertical;
+    }
+}
---
+++ serverRMQExchange/src/main/java/servlet/ChannelPool.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.apache.commons.pool2.BasePooledObjectFactory;
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+
+// ChannelPool based on Apache Commons Pool
+// https://commons.apache.org/proper/commons-pool/examples.html
+public class ChannelPool extends BasePooledObjectFactory<Channel> {
+
+    // private static final String HOST = "127.0.0.1";
+    // private static final String HOST = "http://localhost";
+    private static final String HOST = "3.86.161.121";
+    private static final int PORT = 5672;
+    private final ConnectionFactory connFactory = new ConnectionFactory();
+
+    @Override
+    public Channel create() throws Exception {
+        // Java Client - https://www.rabbitmq.com/api-guide.html
+        // this.connFactory.setHost("localhost");
+
+        this.connFactory.setHost(HOST);
+        this.connFactory.setPort(PORT);
+        this.connFactory.setUsername("admin123456");
+        this.connFactory.setPassword("123456");
+
+        Connection conn = this.connFactory.newConnection();
+        return conn.createChannel();
+    }
+
+    @Override
+    public PooledObject<Channel> wrap(Channel channel) {
+        return new DefaultPooledObject<>(channel);
+    }
+
+    @Override
+    public void destroyObject(PooledObject<Channel> p) throws Exception {
+        p.getObject().close();
+    }
+}
