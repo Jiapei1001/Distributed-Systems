5cfc17f5fe8d9c45bbadea9517b32fe9dcc37bc1
Jiapei1001 <jiapeili@live.com>
2022-04-22 20:35:43 -0700

    transfer getters from skierConsumer to server

--- a3/serverRMQExchange/src/main/java/dao/SkierDao.java
+++ a3/serverRMQExchange/src/main/java/dao/SkierDao.java
@@ -2,6 +2,11 @@ package dao;
 
 
 import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
@@ -49,10 +54,70 @@ public class SkierDao {
             // // Increase this value when you see pool.getResource() taking a long time to complete under burst scenarios
             poolConfig.setMinIdle(64);
 
-            jedisPool = new JedisPool(poolConfig, SKIER_REDIS_HOST, SKIER_REDIS_PORT, 10 * 1000, "default", "admin123456");
+            jedisPool = new JedisPool(poolConfig, SKIER_REDIS_HOST, SKIER_REDIS_PORT, 10 * 1000,
+                    "default", "admin123456");
             // jedisPool = new JedisPool(REDIS_HOST, REDIS_PORT);
         } catch (Exception e) {
             throw new Exception("First create JedisPool error : " + e);
         }
+    }
+
+    // Query #1 - For skier N, how many days have they skied this season?
+    public int getSkiDaysThisSeason(String skierID, String seasonID) {
+        long res = -1;
+
+        String Skier_Season = "Skier_" + skierID + "_Season_" + seasonID;
+        try (Jedis jedis = jedisPool.getResource()) {
+            // set length
+            res = jedis.llen(Skier_Season);
+        }
+
+        return (int) res;
+    }
+
+    // Query #2 - For skier N, what are the vertical totals for each ski day? (calculate vertical as liftID*10)
+    public int getVerticalTotalsPerDay(String skierID, Integer resortID, String seasonID,
+            String dayID) {
+        Integer res = null;
+
+        String Skier_Resort = "Skier_" + skierID + "_Resort_" + resortID;
+        String Season_Day = seasonID + "_" + dayID;
+        try (Jedis jedis = jedisPool.getResource()) {
+            // hash, key, field
+            res = Integer.parseInt(jedis.hget(Skier_Resort, Season_Day));
+        }
+
+        return res;
+    }
+
+    // For skier N, get the total vertical for the skier the specified resort.
+    public Map<String, Integer> getVerticalTotalsPerResort(String skierID, Integer resortID) {
+        Map<String, Integer> res = new HashMap<>();
+
+        Map<String, String> seasonVerticals;
+        String Skier_Resort = "Skier_" + skierID + "_Resort_" + resortID;
+        try (Jedis jedis = jedisPool.getResource()) {
+            // hash, key, field
+            seasonVerticals = jedis.hgetAll(Skier_Resort);
+        }
+
+        for (Map.Entry<String, String> e : seasonVerticals.entrySet()) {
+            res.put(e.getKey(), res.getOrDefault(e.getKey(), 0) + Integer.parseInt(e.getValue()));
+        }
+
+        return res.isEmpty() ? null : res;
+    }
+
+    // Query #3 - For skier N, show me the lifts they rode on each ski day
+    public Set<String> getLifts(String skierID, String seasonID, String dayID) {
+        Set<String> res = new HashSet<>();
+
+        String Skier_Season_Day = "Skier_" + skierID + "_Season_" + seasonID + "_Day_" + dayID;
+        try (Jedis jedis = jedisPool.getResource()) {
+            // set members
+            res = jedis.smembers(Skier_Season_Day);
+        }
+
+        return res;
     }
 }
